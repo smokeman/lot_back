d8e57fbcbad701acfd026cce4f6b308d6037d46d
1 <99762276@qq.com>
2017-10-09 16:52:46 +0800

    b-1009-4

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,9 @@ var user = require('../controllers/user.js')
 var merchant = require('../controllers/merchant.js')
 var wechat = require('wechat')
 var wechatapi = require('wechat-api')
+var OAuth = require('wechat-oauth');
+var client = new OAuth('wx4fe135a46ae46e63', '487d13ed7d3ea392a648359d60febbc5');
+
 
 const config = {
   token:'aoxing',
@@ -13,6 +16,23 @@ const config = {
   appsecret:'487d13ed7d3ea392a648359d60febbc5',
   encodingAESKey:'mrf3yFtmdVr53cUtt9ZF7Ch4PKyEX3ZTysPxrH6OfMv'
 }
+
+
+// var oauthApi = new OAuth('wx4fe135a46ae46e63', '487d13ed7d3ea392a648359d60febbc5', function (openid, callback) {
+//   // 传入一个根据openid获取对应的全局token的方法
+//   // 在getUser时会通过该方法来获取token
+//   fs.readFile(openid +':access_token.txt', 'utf8', function (err, txt) {
+//     if (err) {return callback(err);}
+//     callback(null, JSON.parse(txt));
+//   });
+// }, function (openid, token, callback) {
+//   // 请将token存储到全局，跨进程、跨机器级别的全局，比如写到数据库、redis等
+//   // 这样才能在cluster模式及多机情况下使用，以下为写入到文件的示例
+//   // 持久化时请注意，每个openid都对应一个唯一的token!
+//   fs.writeFile(openid + ':access_token.txt', JSON.stringify(token), callback);
+// });
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -28,8 +48,19 @@ router.use('/sign',wechat(config,function(req,res,next){
     encodingAESKey:'mrf3yFtmdVr53cUtt9ZF7Ch4PKyEX3ZTysPxrH6OfMv'
   }
   var api = new wechatapi(config.appid,config.appsecret);
-  res.reply('11111111');
+  api.getTicket()
+  .then((ticket)=>{
+    console.log(ticket)
+    res.reply(ticket)
+  })
+  // res.reply('11111111');
 
+  // var param = {
+  //   debug: false,
+  //   jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'],
+  //   url: 'http://www.xxx.com'
+  //  };
+  //  api.getJsConfig(param, callback);
 
 }))
 
---
+++ routes/tt.js
@@ -0,0 +1,5 @@
+var OAuth = require('wechat-oauth');
+
+var oauthApi = new OAuth('wx4fe135a46ae46e63', '487d13ed7d3ea392a648359d60febbc5','opxwxw9i2-AtcYCNqY6YNmu5ugkg')
+
+console.log(oauthApi);
