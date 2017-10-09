be6c68e4add46035b3f7331ec8b4f9ca6f90ba84
1 <99762276@qq.com>
2017-10-09 09:20:09 +0800

    b-1009-1

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,37 @@ var lottery = require('../controllers/lottery.js')
 var lotteryDetails = require('../controllers/lotteryDetails.js')
 var user = require('../controllers/user.js')
 var merchant = require('../controllers/merchant.js')
+var wechat = require('wechat')
+var wechatapi = require('wechat-api')
+
+const config = {
+  token:'aoxing',
+  appid:'wx4fe135a46ae46e63',
+  appsecret:'487d13ed7d3ea392a648359d60febbc5',
+  encodingAESKey:'mrf3yFtmdVr53cUtt9ZF7Ch4PKyEX3ZTysPxrH6OfMv'
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/sign',wechat(config,function(req,res,next){
+  var message = req.weixin;
+  var config = {
+    token:'aoxing',
+    appid:'wx4fe135a46ae46e63',
+    appsecret:'487d13ed7d3ea392a648359d60febbc5',
+    encodingAESKey:'mrf3yFtmdVr53cUtt9ZF7Ch4PKyEX3ZTysPxrH6OfMv'
+  }
+  var api = new wechatapi(config.appid,config.appsecret);
+  res.replay('11111111');
+
+
+}))
+
+
+
 
 router.post('/lottery_add',lottery.add)
 
