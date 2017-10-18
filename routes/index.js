7a1a49717e72c69e9d7a29b1f42dd357b62353b4
1 <99762276@qq.com>
2017-10-18 09:36:07 +0800

    1018-1

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,8 @@ var wechat = require('wechat')
 var wechatapi = require('wechat-api')
 var OAuth = require('wechat-oauth');
 var client = new OAuth('wx4fe135a46ae46e63', '487d13ed7d3ea392a648359d60febbc5');
-
+var request = require('request')
+var req_promise = require('request-promise')
 
 const config = {
   token:'aoxing',
@@ -32,12 +33,106 @@ const config = {
 //   fs.writeFile(openid + ':access_token.txt', JSON.stringify(token), callback);
 // });
 
-
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/', function(req, res) {
+  CODE = req.query.code;
+  //生成accesstoken_url的方法
+  var make_accesstoken_url = function(appid, code, secret) {
+      var rooturl = 'https://api.weixin.qq.com/sns/oauth2/access_token';
+      var queryappid = 'appid=' + appid;
+      var querycode = 'code=' + code;
+      var querysecret = 'secret=' + secret;
+      var querygranttype = 'grant_type=authorization_code';
+      return rooturl + '?' + queryappid + '&' + querysecret + '&' + querycode + '&' + querygranttype;
+  }
+  //生成userinfo_url的方法
+  var userinfo_url = function(accesstoken, openid) {
+      var rooturl = 'https://api.weixin.qq.com/sns/userinfo';
+      var queryaccesstoken = 'access_token=' + accesstoken;
+      var queryopenid = 'openid=' + openid;
+      var querylang = 'lang=zh_CN';
+      return rooturl + '?' + queryaccesstoken + '&' + queryopenid + '&' + querylang;
+  }
+  //调用生成accesstoken_url的方法，调用微信api获取access_token
+  var userinfo = {};
+  req_promise(make_accesstoken_url(config.appid, CODE, config.appsecret)).then(
+      (data) => {
+          var obj = JSON.parse(data);
+          OPENID = obj.openid;
+          ACCESSTOKEN = obj.access_token;
+          //调用生成userinfo_url的方法，调用微信api获取用户信息
+          return req_promise(userinfo_url(ACCESSTOKEN, OPENID));
+      }).then(
+      (data) => {
+          var obj = JSON.parse(data);
+          userinfo = obj
+          userinfo.findOne({
+              openid: OPENID
+          }, function(err, msg) {
+              if (err) {
+                  res.send(err);
+                  return console.err(err);
+              }
+              if (!msg) {
+                  var user = new Userinfo({
+                      openid: userinfo.openid,
+                      nickname: userinfo.nickname,
+                      sex: userinfo.sex,
+                      province: userinfo.province,
+                      city: userinfo.city,
+                      country: userinfo.country,
+                      headimgurl: userinfo.headimgurl,
+                      privilege: userinfo.privilege,
+                      unionid: userinfo.unionid
+                  });
+
+                  user.save(function(err, user) {
+                      if (err) return console.log(err, '错误信息');
+                  });
+              }
+              res.render('welecome');
+          })
+      });
 });
+//获取基本信息页
+router.get('/baseinfo', function(req, res) {
+  Userinfo.findOne({
+      openid: OPENID
+  }, function(err, msg) {
+      if (err) return console.err(err);
+
+      res.render('baseinfo', {
+          headimgurl: msg.headimgurl,
+          nickname: msg.nickname,
+          sex: (msg.sex == 1 ? '男' : '女')
+      });
+  });
+})
+
+//获取详细信息页
+router.get('/detailinfo', function(req, res) {
+  Userinfo.findOne({
+      openid: OPENID
+  }, function(err, msg) {
+      if (err) return console.err(err);
+
+      res.render('detailinfo', {
+          nickname: msg.nickname,
+          sex: (msg.sex == 1 ? '男' : '女'),
+          headimgurl: msg.headimgurl,
+          OPENID: msg.openid,
+          country: msg.country,
+          province: msg.province,
+          city: msg.city
+      });
+  });
+})
+
+
 
 router.use('/sign',wechat(config,function(req,res,next){
   var message = req.weixin;
